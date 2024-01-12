e473385b475e1d0685a894497b2aacf2db971c21
Liam Spillane <123liamspillane@gmail.com>
2024-01-12 02:17:33 -0500

    through video 6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="index">
+    <h2>Welcome</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus illum vitae natus corrupti eligendi? Iure quo minima perferendis aspernatur. Suscipit in qui nulla. Unde minus saepe culpa blanditiis atque laborum.</p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores ex corrupti voluptate distinctio beatae. Nostrum iusto minima non fuga exercitationem, corporis ad corrupti molestiae, possimus in quae error dolor eum.</p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
